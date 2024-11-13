1b74294e342e886f7cb2578ef0cd100145f5e2cd
Evaeadao2005 <86280981+Evaeadao2005@users.noreply.github.com>
2024-11-13 02:09:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,39 +15,4 @@ function movePriceSlider(direction) {
   priceIndex = (priceIndex + direction + totalPrices) % totalPrices;
   prices.style.transform = `translateX(-${priceIndex * 100}%)`;
 }
-    // Exibir o popup após 3 segundos
-    setTimeout(() => {
-      document.getElementById("discountPopup").style.display = "block";
-      startCountdown();
-    }, 3000);
-
-    // Função para fechar o popup
-    function closePopup() {
-      document.getElementById("discountPopup").style.display = "none";
-    }
-
-    // Função para calcular o tempo restante até 23:59 e atualizar o cronômetro
-    function startCountdown() {
-      const timerElement = document.getElementById("timer");
-
-      function updateTimer() {
-        const now = new Date();
-        const endOfDay = new Date();
-        endOfDay.setHours(23, 59, 59, 999);
-
-        const timeRemaining = endOfDay - now;
-
-        if (timeRemaining > 0) {
-          const hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
-          const minutes = Math.floor((timeRemaining / (1000 * 60)) % 60);
-          const seconds = Math.floor((timeRemaining / 1000) % 60);
-
-          timerElement.textContent = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-        } else {
-          timerElement.textContent = "00:00:00";
-        }
-      }
-
-      updateTimer();
-      setInterval(updateTimer, 1000);
-    }
+ 
